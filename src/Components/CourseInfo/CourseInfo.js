ca7f9d2800219808d82890f22c0d070714cb2217
siam-26 <siamshahriar390@gmail.com>
2022-10-29 11:08:41 +0600

    added pdf accessable button

--- src/Components/CourseInfo/CourseInfo.js
+++ src/Components/CourseInfo/CourseInfo.js
@@ -2,32 +2,37 @@ import React from 'react';
 import './CourseInfo.css';
 import { Link, useLoaderData } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
+import Pdf from "react-to-pdf";
 
 
 const CourseInfo = () => {
     const courseInfo = useLoaderData();
-    console.log(courseInfo);
+    const ref = React.createRef();
     return (
         <div>
 
-            <header className='courseInfo-header'>
-                <h2>{courseInfo.name}</h2>
-                <p>{courseInfo.shortDetails}</p>
-                <p><small><i>Lecturer: {courseInfo.lecturer}</i></small></p>
 
-                <Button className='pdfbtn' >Pdf</Button>
+            <div ref={ref}>
+                <header className='courseInfo-header'>
+                    <h2>{courseInfo.name}</h2>
+                    <p>{courseInfo.shortDetails}</p>
+                    <p><small><i>Lecturer: {courseInfo.lecturer}</i></small></p>
+                    <Pdf targetRef={ref} filename="code-example.pdf">
+                        {({ toPdf }) => <Button onClick={toPdf} className='pdfbtn' >Pdf</Button>}
+                    </Pdf>
 
-            </header>
 
-            <main>
-                <h1 className='text-center mt-5'>{courseInfo.name}</h1>
-                <img className='courseImg mt-4' src={courseInfo.image} alt="" />
-                <h2 className='description'>Course Description</h2>
-                <p className='courseInfo-p'>{courseInfo.moreDetails}</p>
+                </header>
 
-                <Link to={`/checkout/${courseInfo.id}`}><Button className='premiumAccessBtn' >Get premium access</Button></Link>
-            </main>
+                <main>
+                    <h1 className='text-center mt-5'>{courseInfo.name}</h1>
+                    <img className='courseImg mt-4' src={courseInfo.image} alt="" />
+                    <h2 className='description'>Course Description</h2>
+                    <p className='courseInfo-p'>{courseInfo.moreDetails}</p>
 
+                    <Link to={`/checkout/${courseInfo.id}`}><Button className='premiumAccessBtn' >Get premium access</Button></Link>
+                </main>
+            </div>
         </div>
     );
 };
